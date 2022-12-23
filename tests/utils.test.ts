0f6ef031b1c272553264fdbcfeeae3f6c5c400c5
Jonah <73760377+jonerrr@users.noreply.github.com>
2022-12-23 14:25:17 -0500

    create tests and more utils functions

---
+++ tests/utils.test.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import { describe, expect, it } from "@jest/globals";
+
+import { fetchAliases, generateEmail, generateHash, Settings } from "~utils";
+
+describe("util tests", () => {
+	const settings: Settings = {
+		host: process.env.TEST_MAILCOW_HOST!,
+		apiKey: process.env.TEST_APIKEY!,
+		forwardAddress: process.env.TEST_FORWARD_ADDRESS!,
+		aliasDomain: process.env.TEST_ALIAS_DOMAIN!,
+		generationMethod: 0,
+	};
+
+	it("should generate a hash for example.com", () => {
+		const hash = generateHash("example.com");
+		expect(hash).toBe(
+			"a379a6f6eeafb9a55e378c118034e2751e682fab9f2d30ab13d2125586ce1947",
+		);
+	});
+
+	it("should generate an email for example.com", () => {
+		const email = generateEmail(settings, "example.com");
+
+		expect(
+			email.endsWith(process.env.TEST_ALIAS_DOMAIN!.split("@").pop()),
+		).toBe(true);
+	});
+
+	it("should fetch aliases", async () => {
+		const aliases = fetchAliases(settings);
+		console.log(aliases);
+	});
+});
