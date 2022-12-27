43b063f53817408b091cc0ac905c25811ce61a78
Jonah <73760377+jonerrr@users.noreply.github.com>
2022-12-27 00:01:25 -0500

    continue options rework and open options page onInstall

--- src/options.tsx
+++ src/options.tsx
@@ -2,20 +2,30 @@ import {
   Button,
   Card,
   Center,
+  Collapse,
   Container,
   createStyles,
   Group,
+  Loader,
   MantineProvider,
+  Modal,
   PasswordInput,
+  SegmentedControl,
+  Select,
   Skeleton,
   Text,
   TextInput,
+  Title,
 } from "@mantine/core";
-import { NotificationsProvider } from "@mantine/notifications";
+import { useInterval } from "@mantine/hooks";
+import {
+  NotificationsProvider,
+  showNotification,
+} from "@mantine/notifications";
 import { useStorage } from "@plasmohq/storage/hook";
 import { useEffect, useState } from "react";
 
-import type { Settings } from "~utils";
+import { fetchDomains, Settings } from "~utils";
 
 const useStyles = createStyles((theme) => ({
   item: {
@@ -24,6 +34,11 @@ const useStyles = createStyles((theme) => ({
       marginTop: theme.spacing.sm,
       borderTop: `1px solid ${theme.colors.dark[4]}`,
     },
+  },
+  collapse: {
+    paddingTop: theme.spacing.sm,
+    marginTop: theme.spacing.sm,
+    borderTop: `1px solid ${theme.colors.dark[4]}`,
   },
 }));
 
@@ -34,50 +49,35 @@ export default function IndexOption() {
     "settings",
     "loading"
   );
+  const [initialSetup, setInitialSetup] = useStorage<boolean>(
+    "initialSetup",
+    false
+  );
 
-  console.log(settings);
+  const [loading, setLoading] = useState(false);
 
   // for temporary storage of config values. once they are valid, they are saved in storage
   const [host, setHost] = useState("");
   const [apiKey, setApiKey] = useState("");
   const [forwardAddress, setForwardAddress] = useState("");
-  const [domains, setDomains] = useState<string[]>();
 
-  // load settings from storage only after they have loaded
+  const [domains, setDomains] = useState<string[]>([]);
+
   useEffect(() => {
+    // load settings from storage only after they have loaded
     if (settings === "loading") return;
 
+    // set the state of the config values to the values in storage
     setHost((settings as Settings).host ?? "");
     setApiKey((settings as Settings).apiKey ?? "");
     setForwardAddress((settings as Settings).forwardAddress ?? "");
   }, [settings]);
 
-  // save host if valid
-  useEffect(() => {
-    if (
-      host.match(
-        /^(https?:\/\/)(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/gi
-      ) &&
-      settings !== "loading"
-    )
-      updateSettings({ ...(settings as Settings), host });
-  }, [host]);
-
-  // save API key if valid
-  useEffect(() => {
-    if (apiKey.match(/^([\w]{6}-){4}[\w]{6}$/g) && settings !== "loading")
-      updateSettings({ ...(settings as Settings), apiKey });
-  }, [apiKey]);
-
-  // save forwarding address if valid
-  useEffect(() => {
-    if (
-      forwardAddress.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/g) &&
-      settings !== "loading"
-    )
-      updateSettings({ ...(settings as Settings), forwardAddress });
-  }, [forwardAddress]);
-  // TODO unsaved / saved changes status indicator
+  const changesSaved =
+    settings !== "loading" &&
+    settings.host === host &&
+    settings.apiKey === apiKey &&
+    settings.forwardAddress === forwardAddress;
 
   return (
     <MantineProvider
@@ -103,6 +103,19 @@ export default function IndexOption() {
       }}
     >
       <NotificationsProvider>
+        <Modal
+          opened={initialSetup}
+          centered
+          closeOnClickOutside={false}
+          onClose={() => setInitialSetup(false)}
+          title={<Text fw={700}>Thank you for installing!</Text>}
+        >
+          <Text c="dimmed">
+            Please configure your Mailcow details to use the extension. You can
+            change these settings later in the extension's options.
+          </Text>
+        </Modal>
+
         <Card
           withBorder
           radius="md"
@@ -111,12 +124,20 @@ export default function IndexOption() {
           sx={{ backgroundColor: "dark.7" }}
         >
           <Skeleton visible={settings === "loading"}>
-            <Text size="lg" sx={{ lineHeight: 1 }} weight={700}>
-              Extension Configuration
-            </Text>
-            <Text size="xs" color="dimmed" mt={3} mb="xl">
-              Configure your Mailcow details and alias generation method
-            </Text>
+            <Group position="apart">
+              <div>
+                <Text size="lg" sx={{ lineHeight: 1 }} weight={700}>
+                  Extension Configuration
+                </Text>
+                <Text size="xs" color="dimmed" mt={3} mb="xl">
+                  Configure your Mailcow details and alias generation method
+                </Text>
+              </div>
+
+              <Text c={changesSaved ? "gray.4" : "red.5"}>
+                {changesSaved ? "All changes saved" : "Unsaved changes"}
+              </Text>
+            </Group>
 
             <Group
               position="apart"
@@ -133,7 +154,14 @@ export default function IndexOption() {
               <TextInput
                 value={host}
                 onChange={(event) => {
-                  setHost(event.currentTarget.value);
+                  event.currentTarget.value.match(
+                    /^(https?:\/\/)(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/gi
+                  )
+                    ? updateSettings({
+                        ...(settings as Settings),
+                        host: event.currentTarget.value,
+                      })
+                    : setHost(event.currentTarget.value);
                 }}
                 error={
                   host === "" ||
@@ -163,11 +191,20 @@ export default function IndexOption() {
               </div>
               <PasswordInput
                 value={apiKey}
-                onChange={(event) =>
+                onChange={(event) => {
                   // replace any character that can't be part of an API key.
                   // it is easy to accidentally copy a bunch of spaces or newlines when copying the API key
-                  setApiKey(event.currentTarget.value.replace(/[^\w\d-]+/g, ""))
-                }
+                  const cleaned = event.currentTarget.value.replace(
+                    /[^\w\d-]+/g,
+                    ""
+                  );
+                  cleaned.match(/^([\w]{6}-){4}[\w]{6}$/g)
+                    ? updateSettings({
+                        ...(settings as Settings),
+                        apiKey: cleaned,
+                      })
+                    : setApiKey(cleaned);
+                }}
                 error={
                   apiKey === "" || apiKey.match(/^([\w]{6}-){4}[\w]{6}$/g)
                     ? null
@@ -193,9 +230,16 @@ export default function IndexOption() {
               </div>
               <TextInput
                 value={forwardAddress}
-                onChange={(event) =>
-                  setForwardAddress(event.currentTarget.value)
-                }
+                onChange={(event) => {
+                  event.currentTarget.value.match(
+                    /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/g
+                  )
+                    ? updateSettings({
+                        ...(settings as Settings),
+                        forwardAddress: event.currentTarget.value,
+                      })
+                    : setForwardAddress(event.currentTarget.value);
+                }}
                 error={
                   forwardAddress === "" ||
                   forwardAddress.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/g)
@@ -206,6 +250,107 @@ export default function IndexOption() {
                 size="md"
                 sx={{ width: "30%" }}
               />
+            </Group>
+
+            <Collapse in={!!domains.length} className={classes.collapse}>
+              <Group position="apart" noWrap spacing="xl">
+                <div>
+                  <Text>Alias Domain</Text>
+                  <Text size="xs" color="dimmed">
+                    Default domain used for generating aliases
+                  </Text>
+                </div>
+                <Select
+                  size="md"
+                  sx={{ width: "30%" }}
+                  data={domains}
+                  value={(settings as Settings).aliasDomain}
+                  onChange={(value) =>
+                    updateSettings({
+                      ...(settings as Settings),
+                      aliasDomain: value,
+                    })
+                  }
+                  transition="pop-top-left"
+                  transitionDuration={80}
+                  transitionTimingFunction="ease"
+                />
+              </Group>
+
+              <Group
+                position="apart"
+                noWrap
+                spacing="xl"
+                className={classes.collapse}
+              >
+                <div>
+                  <Text>Generation Method</Text>
+                  <Text size="xs" color="dimmed">
+                    How the alias addresses should be generated
+                  </Text>
+                </div>
+                <SegmentedControl
+                  size="md"
+                  sx={{ width: "30%" }}
+                  data={[
+                    { label: "Random Characters", value: "0" },
+                    { label: "Random Name", value: "1" },
+                    { label: "Website URL", value: "2" },
+                  ]}
+                  value={(settings as Settings).generationMethod?.toString()}
+                  onChange={(value) =>
+                    updateSettings({
+                      ...(settings as Settings),
+                      generationMethod: parseInt(value),
+                    })
+                  }
+                />
+              </Group>
+            </Collapse>
+
+            <Group position="center" mt="sm" pt="sm">
+              <Button
+                variant="light"
+                size="xl"
+                fullWidth
+                loading={loading}
+                disabled={
+                  !(
+                    settings !== "loading" &&
+                    settings.host &&
+                    settings.apiKey &&
+                    settings.forwardAddress
+                  )
+                }
+                onClick={() => {
+                  (async () => {
+                    setLoading(true);
+                    try {
+                      const domains = await fetchDomains(
+                        settings as Required<Settings>
+                      );
+                      if (!domains.length)
+                        return showNotification({
+                          title: "No Domains Found",
+                          message:
+                            "No valid domains were found on your Mailcow instance",
+                          color: "red",
+                        });
+                      setDomains(domains);
+                    } catch (e) {
+                      console.error(e);
+                      showNotification({
+                        title: "Error fetching Mailcow information",
+                        message: e.message,
+                        color: "red",
+                      });
+                    }
+                    setLoading(false);
+                  })();
+                }}
+              >
+                {loading ? "Loading" : "Load"} Mailcow Information
+              </Button>
             </Group>
           </Skeleton>
         </Card>
